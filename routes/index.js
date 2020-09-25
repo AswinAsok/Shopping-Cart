80d039000b602f4e9b3c59f7be451c6979ad5bd4
Aswin Asok <aswinasokofficial@gmail.com>
2020-09-25 15:20:33 +0530

    Part 1 Files Added

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products=[
+    {
+      name: "IPhone 11 Pro",
+      category: "Mobile",
+      description: "This is a good phone",
+      image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-select-2019-family?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1586586488946"
+    },
+    {
+      name: "Oppo Reno 4",
+      category: "Mobile",
+      description:"This is also a good phone",
+      image: "https://i.gadgets360cdn.com/products/large/oppo-2-652x675-1591427929.jpg"
+
+    },
+    {
+      name: "POCO X3",
+      category: "Mobile",
+      description: "This is also a good phone",
+      image: "https://mikqs.com/wp-content/uploads/2020/09/Xiaomi-Poco-X3.png"
+
+    },
+    {
+      name: "Realme V5",
+      category: "Mobile",
+      description: "This is also a good phone",
+      image: "https://cdn1.smartprix.com/rx-iEvD0NonZ-w1200-h1200/realme-v5.jpg"
+    }
+  ]
+  res.render('index', {products,admin: false});
+});
+
+module.exports = router;
