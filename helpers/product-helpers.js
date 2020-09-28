60662b1d2aa042a95a6423eeced398101cad62a0
Aswin Asok <aswinasokofficial@gmail.com>
2020-09-28 14:59:01 +0530

    P on the admin panel

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,5 +1,5 @@
 var db = require('../config/connection')
-
+var collection = require('../config/collections')
 module.exports = {
 
     addProduct:(product,callback)=>{
@@ -8,5 +8,12 @@ module.exports = {
             
             callback(data.ops[0]._id)
         })
+    },
+
+    getAllProducts:()=>{
+         return new Promise(async(resolve,reject)=>{
+             let products =await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+             resolve(products)
+         })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,38 +1,14 @@
 var express = require('express');
+const productHelpers = require('../helpers/product-helpers');
 var router = express.Router();
 var productHelper = require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let products=[
-    {
-      name: "IPhone 11 Pro",
-      category: "Mobile",
-      description: "This is a good phone",
-      image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-select-2019-family?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1586586488946"
-    },
-    {
-      name: "Oppo Reno 4",
-      category: "Mobile",
-      description:"This is also a good phone",
-      image: "https://i.gadgets360cdn.com/products/large/oppo-2-652x675-1591427929.jpg"
-
-    },
-    {
-      name: "POCO X3",
-      category: "Mobile",
-      description: "This is also a good phone",
-      image: "https://mikqs.com/wp-content/uploads/2020/09/Xiaomi-Poco-X3.png"
-
-    },
-    {
-      name: "Realme V5",
-      category: "Mobile",
-      description: "This is also a good phone",
-      image: "https://cdn1.smartprix.com/rx-iEvD0NonZ-w1200-h1200/realme-v5.jpg"
-    }
-  ]
-  res.render('admin/view-products',{admin:true,products})
+  productHelpers.getAllProducts().then((products)=>{
+    console.log(products);
+    res.render('admin/view-products',{admin:true,products})
+  })
 });
 
 router.get('/add-product',(req,res)=>{
