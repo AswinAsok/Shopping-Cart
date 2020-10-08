62f803a1fe30d40b7bca79dea8deb4623d21a7fb
Aswin Asok <aswinasokofficial@gmail.com>
2020-10-08 19:31:40 +0530

    Part 8 Completed

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -62,6 +62,32 @@ module.exports={
                 ))
             }
         })
+    },
+    getCartProducts:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let cartItems = await db.get().collection(collection.CART_COLLECTION).aggregate([
+                {
+                    $match:{user:objectId(userId),}
+                },
+                {
+                    $lookup:{
+                        from:collection.PRODUCT_COLLECTION,
+                        let:{prodList:'$products'},
+                        pipeline:[
+                            {
+                                $match:{
+                                    $expr:{
+                                        $in:['$_id',"$$prodList"]
+                                    }
+                                }
+                            }
+                        ],
+                        as:'cartItems'
+                    }
+                }
+            ]).toArray()
+            resolve(cartItems[0].cartItems)
+        })
     }
 }
      
--- routes/user.js
+++ routes/user.js
@@ -62,7 +62,9 @@ router.get('/logout',(req,res,next)=>{
   res.redirect("/")
 })
 
-router.get("/cart",verifyLogin, (req,res)=>{
+router.get("/cart",verifyLogin, async (req,res)=>{
+  let products = await userHelpers.getCartProducts(req.session.user._id)
+  console.log(products);
   res.render("user/cart")
 })
 
